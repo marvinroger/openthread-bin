c2c32f72384bd9527ee24c550440bf9cb2f10868
Marvin ROGER <bonjour+git@marvinroger.fr>
2020-06-15 20:46:21 +0200

    Add macos support

--- .github/assets/build.sh
+++ .github/assets/build.sh
@@ -13,4 +13,14 @@ cd openthread
 make -f src/posix/Makefile-posix DAEMON=1
 
 # Compress
-tar -zcvf ./build.tar.gz -C ./output/posix/x86_64-unknown-linux-gnu .
+output_directory=""
+if [[ "$OS" = ubuntu* ]]
+then
+    output_directory="x86_64-unknown-linux-gnu"
+elif [[ "$OS" = macos* ]]
+then
+    output_directory="x86_64-apple-darwin"
+else
+    echo "Only ubuntu and macos are supported" && exit 1
+fi
+tar -zcvf ./build.tar.gz -C "./output/posix/${output_directory}" .
