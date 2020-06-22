c8ee4a40cadec61e1d0766cd9cf5bae2605ab14e
Marvin ROGER <bonjour+git@marvinroger.fr>
2020-06-22 21:29:12 +0200

    Remove USB bootloader support

--- .github/assets/build.sh
+++ .github/assets/build.sh
@@ -16,7 +16,7 @@ then
     make -f src/posix/Makefile-posix DAEMON=1
 elif [[ "$build_type" == "nrf52840" ]]
 then
-    make -f examples/Makefile-nrf52840 USB=1 BOOTLOADER=USB
+    make -f examples/Makefile-nrf52840 USB=1
     for file in output/nrf52840/bin/*; do arm-none-eabi-objcopy -O ihex "$file" "$file.hex"; done
 fi
 
