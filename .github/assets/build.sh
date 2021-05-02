eca3bc2d56cfeeab803bff2e9db5331fae0556e0
Marvin ROGER <marvinroger@users.noreply.github.com>
2021-05-02 12:59:25 +0200

    Fix macOS build

--- .github/assets/build.sh
+++ .github/assets/build.sh
@@ -2,22 +2,36 @@
 
 set -euxo pipefail
 
+
+build() {
+    local type="$1"
+    pushd openthread
+
+    # Setup
+    ./script/bootstrap
+    apply_workaround
+    ./bootstrap
+
+    # Build
+    if [[ "$type" == "posix" ]]
+    then
+        make -f src/posix/Makefile-posix DAEMON=1
+    elif [[ "$type" == "nrf52840" ]]
+    then
+        make -f examples/Makefile-nrf52840 USB=1
+        for file in output/nrf52840/bin/*; do arm-none-eabi-objcopy -O ihex "$file" "$file.hex"; done
+    fi
+
+    popd
+}
+
+apply_workaround() {
+    # m4 seems to be missing in ./script/bootstrap
+    if [[ "$TARGET_OS" == macos* ]]
+    then
+        brew install m4
+    fi
+}
+
 build_type="$1"
-
-pushd openthread
-
-# Setup
-./script/bootstrap
-./bootstrap
-
-# Build
-if [[ "$build_type" == "posix" ]]
-then
-    make -f src/posix/Makefile-posix DAEMON=1
-elif [[ "$build_type" == "nrf52840" ]]
-then
-    make -f examples/Makefile-nrf52840 USB=1
-    for file in output/nrf52840/bin/*; do arm-none-eabi-objcopy -O ihex "$file" "$file.hex"; done
-fi
-
-popd
+build "$build_type"
--- .github/assets/entrypoint.sh
+++ .github/assets/entrypoint.sh
@@ -6,26 +6,18 @@ set -euxo pipefail
 build_type="posix"
 env_type="host"
 dockerfile_arch=""
-output_directory=""
 if [[ "$TARGET_OS" == macos* && "$TARGET_ARCH" != "x86_64" ]]
 then
     echo "No op"
     echo "::set-output name=no_op::1"
     exit 0
-elif [[ "$TARGET_OS" == macos* ]]
-then
-    output_directory="x86_64-apple-darwin"
 elif [[ "$TARGET_OS" == ubuntu* && "$TARGET_ARCH" == "aarch64" ]]
 then
     env_type="docker"
     dockerfile_arch="aarch64"
-    output_directory="aarch64-unknown-linux-gnu"
 elif [[ "$TARGET_OS" == ubuntu* && "$TARGET_ARCH" == "nrf52840" ]]
 then
     build_type="nrf52840"
-elif [[ "$TARGET_OS" == ubuntu* ]]
-then
-    output_directory="x86_64-unknown-linux-gnu"
 fi
 
 git clone -n https://github.com/openthread/openthread.git
@@ -35,7 +27,7 @@ popd
 
 if [[ "$env_type" == "host" ]]
 then
-    ./.github/assets/build.sh "${build_type}"
+    ./.github/assets/build.sh "$build_type"
 else
     sudo apt update -y && sudo apt install -y qemu qemu-user-static
     docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
@@ -45,8 +37,8 @@ else
         --rm \
         -v "${PWD}:/github/workspace" \
         -t ot_environment \
-        /bin/bash -c './.github/assets/build.sh "$0"' "${build_type}"
+        /bin/bash -c './.github/assets/build.sh "$0"' "$build_type"
 fi
 
 # Compress
-tar -zcvf ./build.tar.gz -C "./openthread/output/${build_type}" .
+tar -zcvf ./build.tar.gz -C "./openthread/output/$build_type" .
